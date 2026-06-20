00cc75b9248e90403d1253f3909bccce46a2f340
ElGabir <144541672+ElGabir@users.noreply.github.com>
2026-06-20 17:16:41 +0300

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+}
+
+module.exports = nextConfig
